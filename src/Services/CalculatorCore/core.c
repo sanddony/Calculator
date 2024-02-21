fcf21b59d4e78252ec2d3047bb19851436be431d
Sand Donya <sanddony@in-e8.nsk.21-school.ru>
2024-02-21 20:05:08 +0700

    feat: remove c part

--- src/Services/CalculatorCore/core.c
+++
@@ -1,6 +0,0 @@
-#include "calculator.h"
-
-double calculateExpression(const char* expression, int* validateStatus,
-                           double x) {
-  return getResultOfExpression(expression, validateStatus, x);
-}
