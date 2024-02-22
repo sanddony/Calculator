7dbcc88f3dc8023a92474355e30167d927662bd4
Sand Donya <sanddony@in-e8.nsk.21-school.ru>
2024-02-22 18:03:20 +0700

    feat: add some theory in comment

--- src/Services/Calculate.cpp
+++ src/Services/Calculate.cpp
@@ -1,11 +1,53 @@
 #include "Calculate.h"
 
+#include <string>
+
+enum class OperationType{
+  Binary,
+  Unary,
+  OpenBracket,
+  CloseBracket
+};
+
+struct Operation{
+  size_t priority;
+  std::string token;
+  OperationType type;
+};
+
+// OperationBuilder -> OperationList
+  //OperationList
+    // operator[]
+// Lexer(OperationList)
+// Calculator(Lexer)
+  // stack x 2
+  // 
+
+
+// binary operation
+// unary operation
+// brackets
+
+// currentOperation 
+// if(currentOperation == binaryOperation)
+  // while(curr.prority <= top.priority)
+    // caclculate previously operation -> push result to stack
+  // push currentOperation to stack
+// else if(currentOperation == unaryOperation)
+    // push currentOperation to stack
+// else if(currentOperation == openBrackets)
+ // push currentOperation to stack
+// else if(currentOperation == closeBrackets)
+  // while(currentOperation != openBrackets)
+    // caclculate previously operation -> push result to stack
+
+
 double Calculate::operator()(string &expression) {
   int operation = 0;
-  return calculateExpression(expression.c_str(), &operation, 0);
+
 }
 
 double Calculate::operator()(string &expression, double x) {
   int operation = 0;
-  return calculateExpression(expression.c_str(), &operation, x);
+  
 }
--- src/Services/Calculate.h
+++ src/Services/Calculate.h
@@ -1,15 +1,18 @@
 #pragma once
-#include "core.h"
 #include <string>
+#include <vector>
 #include <vector>
 
 using namespace std;
 
-// x,y
-typedef pair<double, double> coordinates;
+// // x,y
+// typedef pair<double, double> coordinates;
 
 class Calculate {
 public:
   double operator()(string &expression);
   double operator()(string &expression, double x);
+
+private: 
+
 };
